6fae3607d6be6da432acc1f7678a13d3826b113c
Osiris30 <andradeivania16@gmail.com>
2025-12-05 13:36:31 -0600

    creacion de modelos, tabla para base de datos y componentes en livewire

---
+++ home/pc-19/proyetos/ac21059_sufi/ac21059_sufi/app/Models/autores.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class autores extends Model
+{
+    /** @use HasFactory<\Database\Factories\AutoresFactory> */
+    use HasFactory;
+}
